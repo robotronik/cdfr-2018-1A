f4a4df744d0497895cabf30c566d3719a9b69761
loibl.vincent@gmail.com <florian210@free.fr>
2018-05-18 09:13:27 +0200

    ajout test radarxl + interface

---
+++ Programmation/radarxl_test_2servos/receive_pc.py
@@ -0,0 +1,54 @@
+# Module de lecture/ecriture du port serie
+from serial import *
+from math import cos, pi, sin
+import matplotlib.pyplot as plt
+
+# Port serie ttyACM0
+# Vitesse de baud : 115200
+# Timeout en lecture : 1 sec
+# Timeout en ecriture : 1 sec
+L=[]                                           #liste des valeurs lues par le capteur
+
+def graphe(L,theta):                           #fonction graphe                       
+    X=[]                                       #abscisses 
+    Y=[]                                       #ordonnees
+    plt.clf()                                  #clear le graphe
+    plt.ion()                                  #affichage dynamique
+    fig=plt.figure(1)                          #creation figure 1
+    mng = plt.get_current_fig_manager()        #affichage plein ecran
+    mng.resize(*mng.window.maxsize())         
+    for i in range(len(L)):                    #parcourir la trame de donnees recues
+        plt.scatter(0,0,s=200,color="grey")    #point central         
+        plt.show()                             #affichage du point central
+        if(L[i].isdigit()):                    #si pas de problemes                            
+            X.append(int(L[i])*cos(int(theta[i])*pi/180))  #ajout x en polaire
+            Y.append(int(L[i])*sin(int(theta[i])*pi/180))  #ajout y en polaire
+            plt.title("!!! Super Mega Radar De La Mort Qui Tue !!!", size='x-large', position=(0.5, 1.03), color = '0.2') #titre
+            plt.axis('equal')                  #forme circulaire (evite les problemes de deformation des axes)
+            plt.axis([0, 0, -230, 230])        #range des axes (force l'axe des y)
+            plt.xlabel('X polaire', size='large')  #titre x
+            plt.ylabel('Y polaire', size='large')  #titre y
+            plt.plot(X,Y,"o:")                 #affichage en pointilles avec points en etoile
+            #plt.plot([0, X[i]], [0, Y[i]])    #affichage du trait "dynamique"
+            fig.canvas.draw()                  #affichage dynamique de la figure
+    plt.show()                                 #affichage
+
+
+with Serial(port="/dev/ttyACM0", baudrate=115200, timeout=1, writeTimeout=1) as port_serie:   #port serie
+    if port_serie.isOpen():
+	while True:
+            ligne = port_serie.readline()                             #lire la ligne de transmission
+            if(len(ligne)>1):                                         #si pas de problemes
+                ligne=ligne[:-1]                                      #virer le \n
+                L=ligne.split('|')                                    #recomposer les valeurs
+                print("Liste :")   
+                print(L)
+                D=L[0].split(',')
+                A=L[1].split(',')        
+                #theta = [t*360/len(L)*pi/180 for t in range(len(L))]  #theta  
+                print("distance :")
+                print(D)
+                print("theta :")
+                print(A)
+                #graphe(D,A)                                       #graphe
+
